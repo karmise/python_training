4a2350c94ca7b68eb38e2c767526331d3d5928a3
Denis Fomchenko <yucant3@gmail.com>
2020-04-26 14:34:11 +0300

    refactor add test by empty value

--- test_add_group.py
+++ test_add_group.py
@@ -10,12 +10,12 @@ def open_home_page(wd):
     wd.get("http://localhost/addressbook/index.php")
 
 
-def login(wd):
+def login(wd, username, password):
     wd.find_element_by_name("user").click()
     wd.find_element_by_name("user").clear()
-    wd.find_element_by_name("user").send_keys("admin")
+    wd.find_element_by_name("user").send_keys(username)
     wd.find_element_by_name("pass").clear()
-    wd.find_element_by_name("pass").send_keys("secret")
+    wd.find_element_by_name("pass").send_keys(password)
     wd.find_element_by_xpath("//input[@value='Login']").click()
 
 
@@ -23,19 +23,19 @@ def open_groups_page(wd):
     wd.find_element_by_link_text("groups").click()
 
 
-def create_group(wd):
+def create_group(wd, name, header, footer):
     # init group creation
     wd.find_element_by_name("new").click()
     # fill group form
     wd.find_element_by_name("group_name").click()
     wd.find_element_by_name("group_name").clear()
-    wd.find_element_by_name("group_name").send_keys("fwegwg")
+    wd.find_element_by_name("group_name").send_keys(name)
     wd.find_element_by_name("group_header").click()
     wd.find_element_by_name("group_header").clear()
-    wd.find_element_by_name("group_header").send_keys("sdsdgsdg")
+    wd.find_element_by_name("group_header").send_keys(header)
     wd.find_element_by_name("group_footer").click()
     wd.find_element_by_name("group_footer").clear()
-    wd.find_element_by_name("group_footer").send_keys("sdgwet")
+    wd.find_element_by_name("group_footer").send_keys(footer)
     # submit group creation
     wd.find_element_by_name("submit").click()
 
@@ -56,9 +56,18 @@ class TestAddGroup(unittest.TestCase):
     def test_add_group(self):
         wd = self.wd
         open_home_page(wd)
-        login(wd)
+        login(wd, username="admin", password="secret")
         open_groups_page(wd)
-        create_group(wd)
+        create_group(wd, name="fwegwg", header="sdsdgsdg", footer="sdgwet")
+        return_to_groups_page(wd)
+        logout(wd)
+
+    def test_add_empty_group(self):
+        wd = self.wd
+        open_home_page(wd)
+        login(wd, username="admin", password="secret")
+        open_groups_page(wd)
+        create_group(wd, name="", header="", footer="")
         return_to_groups_page(wd)
         logout(wd)
 
