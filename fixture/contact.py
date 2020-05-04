a5db3b5dc3edc8ebb02deadfd44ce4ac7ebe3d5a
Denis Fomchenko <yucant3@gmail.com>
2020-05-04 16:40:13 +0300

    add test_del_contact

--- fixture/contact.py
+++ fixture/contact.py
@@ -71,6 +71,12 @@ class ContactHelper:
         wd = self.app.wd
         wd.find_element_by_xpath("(//input[@name='submit'])[2]").click()
 
+    def delete_first_contact(self):
+        wd = self.app.wd
+        wd.find_element_by_name("selected[]").click()
+        wd.find_element_by_xpath("//input[@value='Delete']").click()
+        wd.switch_to_alert().accept()
+
     def return_to_home_page(self):
         wd = self.app.wd
         wd.find_element_by_link_text("home page").click()
