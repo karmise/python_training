835424144caaca3a74e8f839c1c0cb4762b1f00a
Denis Fomchenko <yucant3@gmail.com>
2020-05-04 15:20:55 +0300

    change WD manager to Chrome

--- fixture/application.py
+++ fixture/application.py
@@ -2,11 +2,12 @@ from selenium import webdriver
 from fixture.session import SessionHelper
 from fixture.group import GroupHelper
 from fixture.contact import ContactHelper
+from webdriver_manager.chrome import ChromeDriverManager
 
 
 class Application:
     def __init__(self):
-        self.wd = webdriver.Firefox()
+        self.wd = webdriver.Chrome(ChromeDriverManager().install())
         self.wd.implicitly_wait(30)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
