506220b9ca515828c7174da9efd7d460718edbdb
Denis Fomchenko <yucant3@gmail.com>
2020-05-04 18:25:04 +0300

    add method submit_contact_edition refactor test_edit_contact

--- test/test_edit_contact.py
+++ test/test_edit_contact.py
@@ -3,7 +3,8 @@ from model.contact import Contact
 
 def test_edit_first_contact(app):
     app.session.login(username="admin", password="secret")
-    app.contact.edit_first_contact(
+    app.contact.edit_first_contact()
+    app.contact.fill_contact_page(
         Contact(firstname="nameEdited", middlename="Edited", lastname="Edited", nickname="Edited",
                 title="Edited",
                 company="Edited", address="Edited", home="Edited", mobile="777777",
@@ -13,4 +14,6 @@ def test_edit_first_contact(app):
                 byear="2010",
                 aday="1", amonth="September", ayear="2007", address2="Edited", phone2="Edited",
                 notes="Edited"))
+    app.contact.submit_contact_edition()
+    app.contact.return_to_home_page()
     app.session.logout()
