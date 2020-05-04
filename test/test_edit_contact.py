b2d0f9da600f13caa30c91bbc816d285c41de816
Denis Fomchenko <yucant3@gmail.com>
2020-05-04 17:56:19 +0300

    add test_edit_contact

---
+++ test/test_edit_contact.py
@@ -0,0 +1,16 @@
+from model.contact import Contact
+
+
+def test_edit_first_contact(app):
+    app.session.login(username="admin", password="secret")
+    app.contact.edit_first_contact(
+        Contact(firstname="nameEdited", middlename="Edited", lastname="Edited", nickname="Edited",
+                title="Edited",
+                company="Edited", address="Edited", home="Edited", mobile="777777",
+                work="Edited",
+                fax="Edited",
+                email="edited@komail.com", homepage="www.yandex.com", bday="10", bmonth="December",
+                byear="2010",
+                aday="1", amonth="September", ayear="2007", address2="Edited", phone2="Edited",
+                notes="Edited"))
+    app.session.logout()
