3e1467b5df14d460358854cf5039a6a209bbd721
Denis Fomchenko <yucant3@gmail.com>
2020-05-16 15:02:12 +0300

    add get_group_list to GroupHelper edit group tests

--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -2,12 +2,18 @@ from model.group import Group
 
 
 def test_edit_first_group_name(app):
+    old_groups = app.group.get_group_list()
     if app.group.count() == 0:
         app.group.create(Group(name="test"))
     app.group.edit_first_group(Group(name="Edited"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
 
 
 def test_edit_first_group_header(app):
+    old_groups = app.group.get_group_list()
     if app.group.count() == 0:
         app.group.create(Group(name="test"))
     app.group.edit_first_group(Group(header="headerEdited"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
