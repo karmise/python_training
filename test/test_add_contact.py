7aa42a19378c395f17dee2134929e76060419e9c
Denis Fomchenko <yucant3@gmail.com>
2020-05-25 23:08:45 +0300

    add parametrize to test_add_contact

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,14 +1,38 @@
 # -*- coding: utf-8 -*-
 from model.contact import Contact
+import pytest
+import random
+import string
 
 
-def test_add_contact(app):
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + string.punctuation + " " * 10
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+
+testdata = [Contact(firstname="", middlename="", lastname="", nickname="", title="", company="",
+                    address="", home="", mobile="", work="", fax="", email="", email2="", email3="", homepage="",
+                    bday="", bmonth="-", byear="", aday="", amonth="-", ayear="", address2="",
+                    phone2="", notes="")] + [
+               Contact(firstname=random_string("firstname", 10), middlename=random_string("middlename", 10),
+                       lastname=random_string("lastname", 10), nickname=random_string("nickname", 10),
+                       title=random_string("title", 20), company=random_string("company", 10),
+                       address=random_string("address", 20), home="+7111",
+                       mobile="+72222", work="+7333",
+                       fax="+74444", email=random_string("email", 20), email2=random_string("email2", 10),
+                       email3=random_string("email3", 10),
+                       homepage=random_string("homepage", 10), bday="11",
+                       bmonth="May", byear="2010",
+                       aday="13", amonth="September",
+                       ayear="2020", address2=random_string("address2", 10),
+                       phone2="+755555", notes=random_string("notes", 10), )
+               for i in range(5)
+           ]
+
+
+@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
+def test_add_contact(app, contact):
     old_contacts = app.contact.get_contact_list()
-    contact = Contact(firstname="tqwet", middlename="twet", lastname="sdgsg", nickname="fsdfsdf", title="fasfa",
-                      company="fasfasf", address="asgasg", home="123", mobile="456", work="789", fax="qwrqwr",
-                      email="www.petrovich.ru", email2="www.ozon.ru", email3="www.youtube.com",
-                      homepage="www.google.com", bday="13", bmonth="May", byear="1990",
-                      aday="18", amonth="June", ayear="2005", address2="gagag", phone2="098", notes="votes")
     app.contact.create_new_contact(contact)
     assert len(old_contacts) + 1 == app.contact.count()
     new_contacts = app.contact.get_contact_list()
