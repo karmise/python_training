5c4cbb51ebaf6551daa2eb25e61f7b5db26052a0
Denis Fomchenko <yucant3@gmail.com>
2020-06-06 14:10:35 +0300

    create check_db_connection.py

---
+++ check_db_connection.py
@@ -0,0 +1,13 @@
+import pymysql.cursors
+
+
+connection = pymysql.connect("127.0.0.1", database="addressbook", user="root", password="")
+
+
+try:
+    cursor = connection.cursor()
+    cursor.execute("select * from group_list")
+    for raw in cursor.fetchall():
+        print(raw)
+finally:
+    connection.close
