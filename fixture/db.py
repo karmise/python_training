dedb1671acd1d3f238863eee76afcbb40b8696eb
Denis Fomchenko <yucant3@gmail.com>
2020-06-06 17:14:55 +0300

    add test_db_matches_ui.py (test_group_list)

--- fixture/db.py
+++ fixture/db.py
@@ -1,4 +1,5 @@
 import pymysql.cursors
+from model.group import Group
 
 
 class DbFixture:
@@ -7,7 +8,19 @@ class DbFixture:
         self.user = user
         self.name = name
         self.password = password
-        self.connection = pymysql.connect(host=host, database=user, user=name, password=password)
+        self.connection = pymysql.connect(host=host, database=name, user=user, password=password)
+
+    def get_group_list(self):
+        list = []
+        cursor = self.connection.cursor()
+        try:
+            cursor.execute("select group_id, group_name, group_header, group_footer from group_list")
+            for row in cursor:
+                (id, name, header, footer) = row
+                list.append(Group(id=str(id), name=name, header=header, footer=footer))
+        finally:
+            cursor.close()
+        return list
 
     def destroy(self):
         self.connection.close()
