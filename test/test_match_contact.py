5ae9696cf42da76cf4ca628c5c08b86e2a0051fe
Denis Fomchenko <yucant3@gmail.com>
2020-06-11 21:12:58 +0300

    edit test_match_contact (assert db == ui)

--- test/test_match_contact.py
+++ test/test_match_contact.py
@@ -3,10 +3,17 @@ from model.contact import Contact
 
 
 def test_match_contact_on_home_page(app, db):
-    contact_from_home_page = sorted(app.contact.get_contact_list(), key=Contact.id_or_max)
-    contact_from_db = sorted(db.get_contact_list(), key=Contact.id_or_max)
-    for i in range(len(contact_from_home_page)):
-        assert contact_from_home_page[i] == contact_from_db[i]
+    all_ui_contacts = sorted(app.contact.get_contact_list(), key=Contact.id_or_max)
+    all_db_contacts_from_db = sorted(db.get_contact_list(), key=Contact.id_or_max)
+    i = 0
+    for contact in all_ui_contacts:
+        contact_from_db = all_db_contacts_from_db[i]
+        i = i + 1
+        assert contact.all_phones_from_home_page == merge_phones_like_on_home_page(contact_from_db)
+        assert contact.all_emails_from_home_page == merge_emails_like_on_home_page(contact_from_db)
+        assert contact.firstname == contact_from_db.firstname
+        assert contact.lastname == contact_from_db.lastname
+        assert contact.address == contact_from_db.address
 
 
 def clear(s):
@@ -15,9 +22,9 @@ def clear(s):
 
 def merge_phones_like_on_home_page(contact):
     return "\n".join(filter(lambda x: x != "", map(lambda x: clear(x), filter
-                            (lambda x: x is not None, [contact.home, contact.mobile, contact.work, contact.phone2]))))
+    (lambda x: x is not None, [contact.home, contact.mobile, contact.work, contact.phone2]))))
 
 
 def merge_emails_like_on_home_page(contact):
     return "\n".join(filter(lambda x: x != "", map(lambda x: clear(x), filter
-                            (lambda x: x is not None, [contact.email, contact.email2, contact.email3]))))
+    (lambda x: x is not None, [contact.email, contact.email2, contact.email3]))))
